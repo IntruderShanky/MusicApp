7f95dcb0837f35e42646115b1c1200307f8d18fc
intrudershanky <intrudershanky@outlook.com>
2017-01-29 20:50:38 +0530

    Database Changed

--- Music/settings.py
+++ Music/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Music.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'musicApp',
+        'USER': 'musicuser',
+        'PASSWORD': 'music@app123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
